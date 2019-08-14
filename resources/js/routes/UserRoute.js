e5ddb4c7bb94fea6ac0627ab37b7b9c94ba6b018
pyaesone <pyaesone@softhatch.com>
2019-08-14 08:52:40 +0630

    sidebar update

--- resources/js/routes/UserRoute.js
+++ resources/js/routes/UserRoute.js
@@ -1,22 +1,21 @@
 import AddUser from '../components/User/AddUser.vue'
 import Auth from '../components/User/Auth.vue'
 import EmptyView from '../components/EmptyRouterView.vue';
-import EditUser from '../components/Patient/EditUser.vue';
-import Users from '../components/Patient/AllPatient.vue';
-import EmptyView from '../components/EmptyRouterView.vue';
+// import EditUser from '../components/Patient/EditUser.vue';
+// import Users from '../components/Patient/AllUser.vue';
 
 export const UserRoute = { 
   path: '/users', component:EmptyView, meta:{name:'Users'},
   children:[
-    { 
-      path: '',
-      name: 'alluser', 
-      component: Users, 
-      meta: {
-        name: 'All',
-        link: '/users'
-      }
-    },
+    // { 
+    //   path: '',
+    //   name: 'alluser', 
+    //   component: Users, 
+    //   meta: {
+    //     name: 'All',
+    //     link: '/users'
+    //   }
+    // },
     { 
       path: 'add', 
       name: 'adduser',
@@ -33,16 +32,17 @@ export const UserRoute = {
         name: 'Edit',
         link: ''
       },
-      children: [
-        { 
-          path: '',
-          name: 'chooseuser',  
-          component: EditUser,
-          meta: {
-            name: '',
-            link: ''
-          }
-        },
-    ]}
+      // children: [
+      //   { 
+      //     path: '',
+      //     name: 'chooseuser',  
+      //     component: EditUser,
+      //     meta: {
+      //       name: '',
+      //       link: ''
+      //     }
+      //   },
+      // ]
+    }
   ] 
 };
