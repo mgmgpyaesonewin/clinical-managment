c179cbaa34271c58a8d4ddfb0b4140bd4838c262
PhyuPhyu <phyuphyuthinweb.it@gmail.com>
2019-09-20 23:15:41 +0630

    Created tableSeeder with faker

---
+++ database/seeds/ConsultationsTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ConsultationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create(); 
+        for($i = 0; $i < 3; $i ++):
+            DB::table('consultations')->insert([
+                'title' => 'GP Surgery',
+                'comment' => $faker->text,
+                'patient_id' => 1,
+                'doctor_id' => 1,
+                'appointment_id' => 1,
+            ]);
+        endfor;
+        for($i = 0; $i < 3; $i ++):
+            DB::table('consultations')->insert([
+                'title' => 'GP Surgery',
+                'comment' => $faker->text,
+                'patient_id' => 2,
+                'doctor_id' => 1,
+                'appointment_id' => 1,
+            ]);
+        endfor;
+        for($i = 0; $i < 3; $i ++):
+            DB::table('consultations')->insert([
+                'title' => 'GP Surgery',
+                'comment' => $faker->text,
+                'patient_id' => 3,
+                'doctor_id' => 1,
+                'appointment_id' => 1,
+            ]);
+        endfor;
+    }
+}
