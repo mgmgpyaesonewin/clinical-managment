4cfd4bb76b743297157d15a86ddf4dd5139969ec
pyaesone <pyaesone@softhatch.com>
2019-10-09 11:49:34 +0630

    appointment

---
+++ database/migrations/2019_10_03_072645_create_appointments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('patient_id');
+            $table->string('authorizer_id');
+            $table->string('doctor_id');
+            $table->string('session_id');
+            $table->date('date');
+            $table->string('day_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
---
+++ database/migrations/2019_10_03_072655_create_sessions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->string('start_time');
+            $table->string('end_time');
+            $table->string('day_name');
+            $table->string('interval');
+            $table->string('table_slot');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+}
