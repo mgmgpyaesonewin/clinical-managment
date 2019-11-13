b24d27a281a40179b537365aa88779d6e8a91c72
pyaesone <pyaesone@softhatch.com>
2019-11-13 15:57:14 +0630

    WIP appointment

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,12 +20,13 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'address' => $faker->address,
-        'dob' => '14/12/1995',
+        'dob' => $faker->dateTimeBetween('1990-01-01', '2012-12-31')->format('d/m/Y'),
         'nrc' => '12/SaKhaNa(N)082719',
         'phone' => $faker->phoneNumber,
         'organization' => 'Group of Hospital',
         'gender' => 'male',
         'email' => $faker->email,
-        'password' => bcrypt('abcd1234')
+        'password' => bcrypt('abcd1234'),
+        'hospital_id' => 1
     ];
 });
