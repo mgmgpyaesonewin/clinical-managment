6125fed9c46137358dbae0d79ace4cd4b2ec9a95
pyaesone <pyaesone@softhatch.com>
2019-11-14 16:32:43 +0630

    Add appoinment reservation for user booking

---
+++ app/Http/Controllers/API/AppointmentController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\AppointmentRequest;
+use App\Repositories\Frontend\AppointmentRepository;
+
+class AppointmentController extends Controller
+{
+    public $appointment;
+    public function __construct(AppointmentRepository $appointmentRepo)
+    {
+        $this->middleware('auth:api');
+        $this->appointment = $appointmentRepo;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(AppointmentRequest $request)
+    {   
+        $data = $request->validated();
+        $data['authorizer_id'] = Auth::user()->id;
+        return $this->appointment->create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- tests/Feature/ReservationTest.php
+++ tests/Feature/ReservationTest.php
@@ -7,29 +7,76 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
 use Carbon\Carbon;
+use App\Appointment;
+use App\SessionInterval;
 
 class ReservationTest extends TestCase
 {
-    // use RefreshDatabase;
+    use RefreshDatabase;
     
     /** @test **/
     public function a_patient_can_book_a_doctor()
     {
+        $this->withoutExceptionHandling();
+
         $user = factory(User::class)->create();
         $doctor = factory(User::class)->create();
         $patient = factory(Patient::class)->create();
         $faker = \Faker\Factory::create(); 
         $tomorrow =  Carbon::createFromTimeStamp($faker->dateTimeBetween('now', '+01 days')->getTimestamp());
         $end_time = Carbon::createFromFormat('Y-m-d H:i:s', $tomorrow)->addHours(3);
-        $this->actingAs($user, 'api')->post('/api/sessions', [
-            'date'  =>  $tomorrow->format('d-m-Y'),
-            'start_time'=>  $tomorrow->format('H:i'),
-            'end_time'=> $end_time->format('H:i'),
-            'interval'=> 15,
-            'doctor_id'  =>  $doctor->id,
+        $this->actingAs($user, 'api')->json('POST','/api/sessions', [
+            'date' => $tomorrow->format('d-m-Y'),
+            'start_time' => "12:00",
+            'end_time' => "13:00",
+            'interval' => 15,
+            'intervals' => 
+                array (
+                  0 => 
+                  array (
+                    'booked' => false,
+                    'date' => '2019-11-21',
+                    'doctor_id' => 2,
+                    'time' => '12:00',
+                  ),
+                  1 => 
+                  array (
+                    'booked' => false,
+                    'date' => '2019-11-21',
+                    'doctor_id' => 2,
+                    'time' => '12:15',
+                  ),
+                  2 => 
+                  array (
+                    'booked' => false,
+                    'date' => '2019-11-21',
+                    'doctor_id' => 2,
+                    'time' => '12:30',
+                  ),
+                  3 => 
+                  array (
+                    'booked' => false,
+                    'date' => '2019-11-21',
+                    'doctor_id' => 2,
+                    'time' => '12:45',
+                  ),
+                ),
+            'doctor_id' => $doctor->id,
+            'cmt' => $faker->text
         ]);
-        $response = $this->actingAs($user, 'api')->get('/api/sessions?id=1');
-        $response->dump();
-        $response->assertStatus(200);
+        $sessions = $this->actingAs($user, 'api')->get('/api/sessions?id='.$doctor->id)->getOriginalContent();
+        $selected_session_id = $sessions->first()->id;
+        $selected_session_interval_id = $sessions->first()->slots->first()->id;
+        $response = $this->actingAs($user, 'api')->post('/api/appointments', [
+            'patient_id' => $patient->id,
+            'doctor_id' => $doctor->id,
+            'session_id' => $selected_session_id,
+            'session_interval_id' => $selected_session_interval_id
+        ]);
+        $response->assertStatus(201);
+        $appointment = Appointment::first()->get();
+        $this->assertTrue($appointment->contains('id', 1));
+        $session_interval = SessionInterval::where('id',$selected_session_interval_id)->first()->get();
+        $this->assertTrue($session_interval->contains('id', 1));
     }
 }
