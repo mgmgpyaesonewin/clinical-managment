0a0e9adb2dd8f9bb8dfefa1e516773ed173fe62e
pyaesone <pyaesone@softhatch.com>
2019-11-23 23:04:09 +0630

    Read Code index comment

--- app/ReadCode.php
+++ app/ReadCode.php
@@ -8,12 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ReadCode extends Model
 {
     use Searchable;
-    public function toSearchableArray()
-    {
-        $array = $this->toArray();
-        $new['code']=$array['code'];
-        $new['title']=$array['title'];
-        return $new;
-    }
+    // public function toSearchableArray()
+    // {
+    //     $array = $this->toArray();
+    //     $new['code']=$array['code'];
+    //     $new['title']=$array['title'];
+    //     return $new;
+    // }
     protected $fillable=['code','block_id','title','class_kind','depth_in_kind','is_residual','chapter_no','is_leaf','no_of_non_residual_children'];
 }
