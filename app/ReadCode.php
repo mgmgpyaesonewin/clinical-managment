c7e375e0da21a87bb217a592a6661510718d4b3a
pkk2332 <babyking67@gmail.com>
2020-01-23 16:20:13 +0630

    asd

--- app/ReadCode.php
+++ app/ReadCode.php
@@ -13,6 +13,7 @@ class ReadCode extends Model
         $array = $this->toArray();
         $new['code']=$array['code'];
         $new['title']=$array['title'];
+        $new[$this->getKeyName()] = $this->getKey();
         return $new;
     }
     protected $fillable=['code','block_id','title','class_kind','depth_in_kind','is_residual','chapter_no','is_leaf','no_of_non_residual_children'];
